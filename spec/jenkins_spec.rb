cb3ed352253defdc62996565583c1473360d0915
FGtatsuro <fujiistorage@gmail.com>
2016-04-04 18:23:45 +0900

    Add specs for Jenkins.
    In OSX, latest Jenkins is installed. Thus, we can't check specified version.

--- spec/jenkins_spec.rb
+++ spec/jenkins_spec.rb
@@ -1,17 +1,13 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe package('python-dev'), :if => ['debian', 'alpine'].include?(os[:family]) do
+describe package('jenkins'), :if => os[:family] == 'darwin' do
   it { should be_installed }
 end
 
-describe package('build-essential'), :if => os[:family] == 'debian' do
-  it { should be_installed }
-end
-
-describe package('build-base'), :if => os[:family] == 'alpine' do
-  it { should be_installed }
-end
-
-describe command('which python') do
+describe command("java -jar /usr/local/opt/jenkins/libexec/jenkins.war --version | tail -n 1") :if => os[:family] == 'darwin' do
   its(:exit_status) { should eq 0 }
+end
+
+describe command("java -jar #{ENV['JENKINS_WAR_PATH']} --version | tail -n 1"), :if => ['debian', 'alpine'].include?(os[:family]) do
+  its(:stdout) { should contain(ENV['JENKINS_VERSION']) }
 end
