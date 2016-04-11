55a789d8dda0b791a68f70bc2dc4b82bfe2a7db7
FGtatsuro <fujiistorage@gmail.com>
2016-04-11 23:39:12 +0900

    Update specs.
    - On OSX, it's difficult to check specified version.
    - On OSX, Homebrew and this role don't create jenkins user/group.
    - On Linux, dedicated jenkins user/group are created.

--- spec/jenkins_spec.rb
+++ spec/jenkins_spec.rb
@@ -4,10 +4,27 @@ describe package('jenkins'), :if => os[:family] == 'darwin' do
   it { should be_installed }
 end
 
-describe command("java -jar /usr/local/opt/jenkins/libexec/jenkins.war --version | tail -n 1") :if => os[:family] == 'darwin' do
+describe command("java -jar #{ENV['JENKINS_WAR_PATH']} --version"), :if => os[:family] == 'darwin' do
+  # On OSX, it's difficult to check specified version because Homebrew installs latest one.
   its(:exit_status) { should eq 0 }
 end
 
 describe command("java -jar #{ENV['JENKINS_WAR_PATH']} --version | tail -n 1"), :if => ['debian', 'alpine'].include?(os[:family]) do
   its(:stdout) { should contain(ENV['JENKINS_VERSION']) }
+end
+
+describe user('jenkins'), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should belong_to_group 'jenkins' }
+end
+
+describe user('jenkins'), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should have_home_directory ENV['JENKINS_HOME'] }
+end
+
+describe user('jenkins'), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should have_login_shell '/bin/sh' }
+end
+
+describe user('jenkins'), :if => ['debian', 'alpine'].include?(os[:family]) do
+  its(:encrypted_password) { should eq "$6$rounds=656000$jenkinsrolegene$NH3.2ots3RJ3wK2xqZgH.R13qW9Q4DV0efbjcijs7ogwNLs0HWB5sq1oCMiswEFbiitIO.B5ZvesPNNPcez/K/" }
 end
